00dcec91b3d691c208bfffa15c07504ee65b7ca9
Eric Pedley <ericpedley@gmail.com>
2025-08-28 21:11:53 -0700

    reformulate entire arm optimization as single problem

--- src/kscale_vr_teleop/arm_inverse_kinematics.py
+++ src/kscale_vr_teleop/arm_inverse_kinematics.py
@@ -20,59 +20,6 @@ right_arm_links = [
 	'KB_C_501X_Right_Bayonet_Adapter_Hard_Stop'
 ]
 
-class IKSolver:
-    def __init__(self, robot: URDF):
-        self.robot = robot
-        self.last_guess = np.zeros(len(robot.actuated_joints)//2)
-        self.lower_bounds = []
-        self.upper_bounds = []
-        for joint in self.robot.actuated_joints[::2]:
-            self.lower_bounds.append(joint.limit.lower)
-            self.upper_bounds.append(joint.limit.upper)
-
-    def from_scratch_ik(self, target_mat, frame_name, initial_guess = None): # This shouldn't be necessary but ikpy's inverse kinematics is ironically crap
-        config_base = {
-                k.name: 0 for k in self.robot.actuated_joints
-            }
-        def residuals(joint_angles):
-            config_update = {
-                k.name: joint_angles[i] for i, k in enumerate(self.robot.actuated_joints[::2] if 'Right' in frame_name else self.robot.actuated_joints[1::2])
-            }
-            config_base.update(config_update)
-            self.robot.update_cfg(config_base)
-            ee_position = self.robot.get_transform(frame_name, "base")
-            ee_forward = -ee_position[:3, 2] if 'Right' in frame_name else ee_position[:3, 2]
-            target_forward = -target_mat[:3, 2]
-            ee_up = -ee_position[:3, 1] if 'Right' in frame_name else ee_position[:3,1]
-            target_up = target_mat[:3,1] 
-            rotation_angle_off = np.arccos(np.dot(ee_forward, target_forward))
-            y_angle_off = np.arccos(np.dot(ee_up, target_up))
-            return np.concatenate([
-                ee_position[:3, 3] - target_mat[:3, 3],
-                [0.1*rotation_angle_off, 0.1*y_angle_off]
-            ])
-        jac_sparsity_mat = np.zeros((5, len(self.robot.actuated_joints)//2))
-        jac_sparsity_mat[:,0] = 1
-        jac_sparsity_mat[:,1] = 1
-        jac_sparsity_mat[:,2] = 1
-        jac_sparsity_mat[:,3] = 1
-        jac_sparsity_mat[4,4] = 1
-
-        SOLVE_WITH_BOUNDS = True
-        result = least_squares(
-            residuals, 
-            self.last_guess, 
-            bounds=(self.lower_bounds, self.upper_bounds) if SOLVE_WITH_BOUNDS else (-np.inf, np.inf), 
-            jac_sparsity=jac_sparsity_mat,
-            xtol=1e-3,
-            gtol=1e-5,
-            ftol=1e-2,
-        )
-        solution = result.x
-        self.last_guess = solution
-        return solution
-
-
 from kscale_vr_teleop._assets import ASSETS_DIR
 
 file_absolute_parent = Path(__file__).parent.absolute()
@@ -97,12 +44,69 @@ if VISUALIZE:
     visualizer.start_viewer()
     visualizer.add_marker('goal', [0.,0.,0.])
 
-right_solver = IKSolver(arms_robot)
-left_solver = IKSolver(arms_robot)
+lower_bounds = []
+upper_bounds = []
+for joint in arms_robot.actuated_joints:
+    lower_bounds.append(joint.limit.lower)
+    upper_bounds.append(joint.limit.upper)
 
+last_guess = np.zeros(10)
 def calculate_arm_joints(head_mat, left_wrist_mat, right_wrist_mat, initial_guess=None):
-    right_joint_angles = right_solver.from_scratch_ik(target_mat=right_wrist_mat, frame_name = 'KB_C_501X_Right_Bayonet_Adapter_Hard_Stop', initial_guess = initial_guess)
-    left_joint_angles = left_solver.from_scratch_ik(target_mat=left_wrist_mat, frame_name = 'KB_C_501X_Left_Bayonet_Adapter_Hard_Stop', initial_guess = initial_guess)
+    global last_guess
+    config_base = {
+            k.name: 0 for k in arms_robot.actuated_joints
+        }
+    def residuals(joint_angles):
+        config_update = {
+            k.name: joint_angles[i] for i, k in enumerate(arms_robot.actuated_joints)
+        }
+        config_base.update(config_update)
+        arms_robot.update_cfg(config_base)
+        right_ee_position = arms_robot.get_transform('KB_C_501X_Right_Bayonet_Adapter_Hard_Stop', "base")
+        left_ee_position = arms_robot.get_transform('KB_C_501X_Left_Bayonet_Adapter_Hard_Stop', "base")
+        right_ee_forward = -right_ee_position[:3,2]
+        left_ee_forward = left_ee_position[:3,2]
+        right_target_forward = -right_wrist_mat[:3, 2]
+        left_target_forward = -left_wrist_mat[:3, 2]
+        right_ee_up = -right_ee_position[:3, 1]
+        left_ee_up = left_ee_position[:3, 1]
+        right_target_up = right_wrist_mat[:3,1]
+        left_target_up = left_wrist_mat[:3,1]
+        right_rotation_angle_off = np.arccos(np.dot(right_ee_forward, right_target_forward))
+        left_rotation_angle_off = np.arccos(np.dot(left_ee_forward, left_target_forward))
+        right_y_angle_off = np.arccos(np.dot(right_ee_up, right_target_up))
+        left_y_angle_off = np.arccos(np.dot(left_ee_up, left_target_up))
+        return np.concatenate([
+            right_ee_position[:3, 3] - right_wrist_mat[:3, 3],
+            left_ee_position[:3, 3] - left_wrist_mat[:3, 3],
+            [0.1*right_rotation_angle_off, 0.1*right_y_angle_off, 0.1*left_rotation_angle_off, 0.1*left_y_angle_off]
+        ])
+    # 0-2: right position
+    # 3-5: left position
+    # 6: right direction
+    # 7: right rotation
+    # 8: left direction
+    # 9: left rotation
+    jac_sparsity_mat = np.zeros((10, len(arms_robot.actuated_joints)))
+    # # joints: evens are right, odds are left, order is shoulder to wrist
+    jac_sparsity_mat[:3, :8:2] = 1
+    jac_sparsity_mat[6:8, ::2] = 1
+    jac_sparsity_mat[3:6, 1:9:2] = 1
+    jac_sparsity_mat[8:10, 1::2] = 1
+
+    result = least_squares(
+        residuals, 
+        last_guess, 
+        bounds=(lower_bounds, upper_bounds),
+        jac_sparsity=jac_sparsity_mat,
+        xtol=1e-3,
+        gtol=1e-3,
+        ftol=1e-3,
+    )
+    solution = result.x
+    last_guess = solution
+    right_joint_angles = solution[::2]
+    left_joint_angles = solution[1::2]
     new_config={
         k.name: right_joint_angles[i] for i, k in enumerate(arms_robot.actuated_joints[::2])
     }
