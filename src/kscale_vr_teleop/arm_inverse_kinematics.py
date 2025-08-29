show ee frames in viz and fix wrist rotation(?)

src/kscale_vr_teleop/arm_inverse_kinematics.py
@@ -41,28 +41,32 @@ class IKSolver:
             config_base.update(config_update)
             self.robot.update_cfg(config_base)
             ee_position = self.robot.get_transform(frame_name, "base")
-            ee_forward = ee_position[:3, 2]
-            target_forward = target_mat[:3, 2] if 'Right' in frame_name else -target_mat[:3, 2]
+            ee_forward = -ee_position[:3, 2] if 'Right' in frame_name else ee_position[:3, 2]
+            target_forward = -target_mat[:3, 2]
+            ee_up = -ee_position[:3, 1] if 'Right' in frame_name else ee_position[:3,1]
+            target_up = target_mat[:3,1] 
             rotation_angle_off = np.arccos(np.dot(ee_forward, target_forward))
+            y_angle_off = np.arccos(np.dot(ee_up, target_up))
             return np.concatenate([
                 ee_position[:3, 3] - target_mat[:3, 3],
-                [0.1*rotation_angle_off]
+                [0.1*rotation_angle_off, 0.1*y_angle_off]
             ])
-        jac_sparsity_mat = np.zeros((1, len(self.robot.actuated_joints)//2))
-        jac_sparsity_mat[0,0] = 1
-        jac_sparsity_mat[0,1] = 1
-        jac_sparsity_mat[0,2] = 1
-        jac_sparsity_mat[0,3] = 1
+        jac_sparsity_mat = np.zeros((5, len(self.robot.actuated_joints)//2))
+        jac_sparsity_mat[0:4,0] = 1
+        jac_sparsity_mat[0:4,1] = 1
+        jac_sparsity_mat[0:4,2] = 1
+        jac_sparsity_mat[0:4,3] = 1
+        jac_sparsity_mat[4,4] = 0
 
         SOLVE_WITH_BOUNDS = True
         result = least_squares(
             residuals, 
             self.last_guess, 
             bounds=(self.lower_bounds, self.upper_bounds) if SOLVE_WITH_BOUNDS else (-np.inf, np.inf), 
-            jac_sparsity=np.repeat(jac_sparsity_mat, 4, axis=0),
-            xtol=1e-5,
+            jac_sparsity=jac_sparsity_mat,
+            xtol=1e-3,
             gtol=1e-5,
-            ftol=1e-5,
+            ftol=1e-2,
         )
         solution = result.x
         self.last_guess = solution

src/kscale_vr_teleop/main.py
@@ -53,7 +53,7 @@ else:
 left_arm_joints = np.zeros(5)
 right_arm_joints = np.zeros(5)
 
-STREAM = True
+STREAM = False
 
 base_to_head_transform = np.eye(4)
 base_to_head_transform[:3,3] = np.array([
@@ -82,6 +82,12 @@ async def stream_cameras(session: VuerSession, left_src=0, right_src=1):
 
         new_config = {k.name: right_arm_joints[i] for i, k in enumerate(arms_robot.actuated_joints[::2])}
         new_config.update({k.name: left_arm_joints[i] for i, k in enumerate(arms_robot.actuated_joints[1::2])})
+
+        arms_robot.update_cfg(new_config)
+        hand_pose_right = arms_robot.get_transform("KB_C_501X_Right_Bayonet_Adapter_Hard_Stop", "base")
+        hand_pose_left = arms_robot.get_transform("KB_C_501X_Left_Bayonet_Adapter_Hard_Stop", "base")
+        rr.log("hand_position_right", rr.Transform3D(translation=hand_pose_right[:3, 3], mat3x3=hand_pose_right[:3, :3], axis_length=0.1))
+        rr.log("hand_position_left", rr.Transform3D(translation=hand_pose_left[:3, 3], mat3x3=hand_pose_left[:3, :3], axis_length=0.1))
 
         urdf_logger.log(new_config)
         if STREAM: